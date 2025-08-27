aa8963e042e91e1fc151e2b098bae4a1f04247c1
tinhbe <98801099+tvantinh@users.noreply.github.com>
2025-08-27 18:02:51 +0700

    update config project

--- src/app/+shell/ui/components/header/feature/header.component.ts
+++ src/app/+shell/ui/components/header/feature/header.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { PortalHostDirective } from "../../../../../../../node_modules/@angular/cdk/portal/index";
 @Component({
   selector: 'meu-header',
   standalone: true,
-  imports: [CommonModule, PortalHostDirective],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
